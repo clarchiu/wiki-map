760b00a6c955108d412ab53c0d23dca39bc17ddd
Clarence Chiu <clarchiu@gmail.com>
2020-11-28 07:20:31 +0000

    added comments

--- db/helpers/maps-get.js
+++ db/helpers/maps-get.js
@@ -1,3 +1,7 @@
+/**
+ * Get all map data from db and return as Promise
+ * @param {*} db
+ */
 const getAllMaps = (db) => {
   return db.query(`SELECT * FROM maps`)
     .then((res) => {
@@ -6,7 +10,14 @@ const getAllMaps = (db) => {
     .catch(() => { msg: "Could not get maps from database" });
 };
 
-const getPinsByMap = (db, map) => {
+/**
+ * Private (for now) - to be used by getMapById only
+ * Given a map object, query its pins from db and append result as an entry
+ * in the map object and return as Promise
+ * @param {*} db
+ * @param {*} map
+ */
+const _getPinsByMap = (db, map) => {
   return db.query(`SELECT * FROM pins WHERE map_id = $1`, [map.id])
     .then(res => {
       map.pins = res.rows;
@@ -14,10 +25,17 @@ const getPinsByMap = (db, map) => {
     });
 }
 
+/**
+ * Given a map id, query the db for its data
+ * Then chain together with _getPinsByMap to add pins data as an array in the object
+ * Return as Promise
+ * @param {*} db
+ * @param {*} id
+ */
 const getMapById = (db, id) => {
   return db.query(`SELECT * FROM maps WHERE id = $1`, [id])
     .then(res => res.rows[0])
-    .then(map => getPinsByMap(db, map))
+    .then(map => _getPinsByMap(db, map))
     .catch(() => { msg: `Could not get map with id: ${id} from database` });
 };
 
