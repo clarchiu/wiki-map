95cf2b0b8df54c850bb25173aff378059f78beac
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-28 02:59:04 +0000

    Create schema for database

---
+++ db/schema/03_pins.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS pins CASCADE;
+
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  map_id INTEGER REFERENCES maps(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  img_url VARCHAR(255),
+  latitude FLOAT NOT NULL,
+  longitude FLOAT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
