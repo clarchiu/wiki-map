0982b041d2179f905dbd3d5ef47e74205051d819
Benjamin Lee <benjaminjslee@gmail.com>
2020-12-01 00:47:36 +0000

    Remove boundaries for maps and zoom restriction

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -109,11 +109,11 @@ $( function() {
 
   const addPinnedMap = function(data) {
     const map = L.map('mapid',{
-      minZoom: 10,
+      minZoom: 1,
     }).setView([data.lat,data.long],data.zoom);
 
-    const bounds = map.getBounds().pad(0.1);
-    map.setMaxBounds([bounds.getSouthWest(),bounds.getNorthEast()]);
+    // const bounds = map.getBounds().pad(0.1);
+    // map.setMaxBounds([bounds.getSouthWest(),bounds.getNorthEast()]);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
