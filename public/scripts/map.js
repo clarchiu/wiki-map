68ab4faa95e7dcac4fba215c87e969b4462f510b
clarchiu <clarchiu@gmail.com>
2020-11-30 16:11:07 -0800

    map_show properly shows map at user specified zoom

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -29,7 +29,7 @@ $( function() {
   }
 
   const addPinnedMap = function(data) {
-    const map = L.map('mapid').setView([data.lat,data.long],10);
+    const map = L.map('mapid').setView([data.lat,data.long],data.zoom);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
