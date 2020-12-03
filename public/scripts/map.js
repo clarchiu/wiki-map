16e77e0f403154e10add9b88eaa29f9b676646ac
Benjamin Lee <benjaminjslee@gmail.com>
2020-12-03 07:11:21 +0000

    Small change to clearing event listeners in map.js

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -93,8 +93,8 @@ $( function() {
   };
 
   const submitPinHandler = function(marker, pinList, mapData, index, coord = null) {
-  $('form.pin-submit').on('submit', function(event) {
-      $('form.pin-delete').off("submit");
+  $('button.delete').on('submit', function(event) {
+      $('form.pin-delete').off("click");
       event.preventDefault();
       event.stopPropagation();
       const sData = `${$(event.target).serialize()}${coord ? `&lat=${coord.lat}&long=${coord.long}` : ""}`;
