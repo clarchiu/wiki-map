b64a1090ea7528c7e7aef17f6ff1a326843b4d76
clarchiu <clarchiu@gmail.com>
2020-12-02 23:49:44 -0800

    fixed bug: selector too specifc for favorite event

--- public/scripts/favorite.js
+++ public/scripts/favorite.js
@@ -8,6 +8,7 @@ $( function() {
    * @returns a promise, containing either an error or undefined
    */
   const favorite = function($target, url) {
+    console.log(url);
     return $.ajax({
       method: 'post',
       url: url,
@@ -21,7 +22,7 @@ $( function() {
     });
   };
 
-  $('#all-maps').on('click', '[data-action]', function(e) {
+  $('.maps').on('click', '[data-action]', function(e) {
     const $span = $(this);
     const url = $span.attr('data-action');
 
